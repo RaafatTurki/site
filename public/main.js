71eceff096c03a4948fcf1a4e9e00a965b76d834
Raafat Turki <raafat.turki@proton.me>
2023-12-24 21:44:12 +0300

    feat: add different pattern

--- public/main.js
+++ public/main.js
@@ -1,60 +1,72 @@
-let w = 30
+let w = 25
 let dots = []
 let rows
 let cols
 
-class Dot {
-    constructor(x, y) {
-        this.pos = createVector(x + (w/2), y + (w/2))
-        this.r = 0
-        this.min_r = 0 
-        this.max_r = (w/3) * 2
-        this.col = 'white'
-    }
-    
-    render() {
-        if (dist(this.pos.x, this.pos.y, mouseX, mouseY) < 100) {
-            // this.col = 'red'
-            this.r = constrain(this.r + 3, this.min_r, this.max_r)
-        // //     offset = 50
-        } else {
-            this.r = constrain(this.r - 1, this.min_r, this.max_r)
-            // this.col = 'white'
-        // //     offset = 0
-        }
+const body = document.querySelector('body')
 
-        fill(this.col)
-        ellipse(this.pos.x, this.pos.y, this.r)
+class Dot {
+  constructor(x, y) {
+    this.pos = createVector(x + (w/2), y + (w/2))
+    this.r = 0
+    this.min_r = 0
+    this.max_r = (w/3) * 2
+    this.a = 100
+    this.min_a = 0
+    this.max_a = 255
+  }
+
+  render() {
+    if (dist(this.pos.x, this.pos.y, mouseX, mouseY) < 100) {
+      this.r = constrain(this.r + 3, this.min_r, this.max_r)
+      this.a = constrain(this.a + 30, this.min_a, this.max_a)
+    } else {
+      this.r = constrain(this.r - 1, this.min_r, this.max_r)
+      this.a = constrain(this.a - 3, this.min_a, this.max_a)
     }
+
+    fill(255, 255, 255, this.a)
+    ellipse(this.pos.x, this.pos.y, this.r)
+  }
 }
 
 function init_dots() {
-    rows = height / w
-    cols = width / w
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            dots.push(new Dot(i * w, j * w))
-        }
+  rows = height / w
+  cols = width / w
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      if ((i + j) % 2 == 0) {
+        dots.push(new Dot(i * w, j * w))
+      }
     }
+  }
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight)
+  init_dots()
+
+  const resize_observer = new ResizeObserver(() => {
+    dots = []
+    resizeCanvas(windowWidth, windowHeight)
     init_dots()
+  })
+
+  resize_observer.observe(body)
 }
 
 function draw() {
-	background("black");
-	noStroke();
+  background("black")
+  noStroke()
 
-    for (let i = 0; i < dots.length; i++) {
-        dots[i].render()
-    }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].render()
+  }
 }
 
-function windowResized() {
-  // resizeCanvas(windowWidth, windowHeight);
-    dots = []
-    resizeCanvas(windowWidth, windowHeight);
-    init_dots()
-}
+// function windowResized() {
+//   // resizeCanvas(windowWidth, windowHeight)
+//     dots = []
+//     resizeCanvas(windowWidth, windowHeight)
+//     init_dots()
+// }
